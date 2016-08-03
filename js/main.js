65b68cdb416a03eede50ea4b89b94898c6029d00
Olivia Mack-Solden <omacksolden@gmail.com>
2016-08-02 20:34:48 -0400

    Adjust menu elements

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 $(function ($) {
     $('.page').fixedBG();
 });
+
+var selectedElement = document.getElementById( 'menucontainer' );
+var newDOMElement = document.createElement( 'h1' );
+
+newDOMElement.innerHTML = 'MENU';
+
+selectedElement.prepend( newDOMElement );
