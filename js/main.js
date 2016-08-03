6b9dec7d91f2c3174c991bd6adc532fbd9487048
Olivia Mack-Solden <omacksolden@gmail.com>
2016-08-02 22:51:24 -0400

    Adjust smallest media query

--- js/main.js
+++ js/main.js
@@ -2,9 +2,9 @@ $(function ($) {
     $('.page').fixedBG();
 });
 
-var selectedElement = document.getElementById( 'menucontainer' );
-var newDOMElement = document.createElement( 'h1' );
-
-newDOMElement.innerHTML = 'MENU';
-
-selectedElement.prepend( newDOMElement );
+// var selectedElement = document.getElementById( 'menucontainer' );
+// var newDOMElement = document.createElement( 'h1' );
+//
+// newDOMElement.innerHTML = 'Come and See Us!';
+//
+// selectedElement.appendChild( newDOMElement );
