31fcc8461a9aea7b6d5743df88224c81d1ac7149
Olivia Mack-Solden <omacksolden@gmail.com>
2016-08-04 23:48:51 -0400

    Rearrange javascript functions and add comments

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
+$(document).ready(function () {
+    $('.owl-carousel').owlCarousel();
+});
+//This jQuery plugin creates an image slider on index
+
 $(function ($) {
     $('.page').fixedBG();
 });
-
+//This jQuery plugin will allow the background image to be fixed
 
 $(document).ready(function () {
     $('.pic').hover(
@@ -12,6 +17,8 @@ $(document).ready(function () {
         $('.hoverpic').animate({'top': '+=200px' }, 'slow');
     });
 });
+//This custom jQuery code causes an image to slide over another on hover
 
 var element = document.getElementById('menu');
 element.innerHTML = 'MENU';
+// This custom javascript code will change the h1 on the menu page to MENU
